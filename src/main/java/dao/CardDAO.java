3479d6c8033029230d8d9f835978f808740f52e4
Zhou,Wei-qiang W.G <w.zhou@student.fontys.nl>
2017-11-28 11:40:20 +0100

    dao gemaakt

--- src/main/java/dao/CardDAO.java
+++ src/main/java/dao/CardDAO.java
@@ -1,4 +1,14 @@
 package dao;
 
-public class CardDAO {
+import models.Card;
+
+import java.util.List;
+
+public interface CardDAO {
+
+    boolean create(Card card);
+    boolean edit(Card card);
+    boolean delete(Card card);
+    Card findById(int id);
+    List<Card> findAll();
 }
