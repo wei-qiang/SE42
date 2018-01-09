5202e00ab719cb0d91b94c1e529211a10d681681
Bergh,Danny M.A. van den <danny.vandenbergh@student.fontys.nl>
2018-01-09 13:54:31 +0100

    Cardimport

---
+++ src/main/java/hello/CardImport.java
@@ -0,0 +1,31 @@
+package hello;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
+
+public class CardImport {
+    public CardImport() throws IOException {
+        zooi();
+    }
+    public void zooi() throws IOException {
+        String s = "https://api.hearthstonejson.com/v1/18336/enUS/cards.collectible.json";
+
+        URL url = new URL(s);
+        Scanner scanner = new Scanner(url.openStream());
+
+        //haalt json string op
+        while(scanner.hasNext()){
+            Scanner scan = new Scanner(url.openStream());
+            String str = new String();
+            while (scan.hasNext())
+                str += scan.nextLine();
+            scan.close();
+        }
+        //maakt json string object.
+        
+    }
+}
