fe103dc644d78def97fa0703e4c9d039a6fc5694
Zhou,Wei-qiang W.G <w.zhou@student.fontys.nl>
2018-01-09 13:55:40 +0100

    json test gemaakt en getcollection

--- src/main/java/hello/AccountController.java
+++ src/main/java/hello/AccountController.java
@@ -1,15 +1,21 @@
 package hello;
 
 import models.Account;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import models.Card;
+import models.User;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.WebUtils;
 import service.CardMgr;
 import service.PasswordHasher;
 import service.UserMgr;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.nio.file.AccessDeniedException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,19 +35,48 @@ public class AccountController {
     }
 
     @RequestMapping("/login")
-    public String loginAccount(HttpServletResponse response, @RequestParam("userName") String userName, @RequestParam("password") String password) {
+    public String loginAccount(HttpServletResponse response, HttpServletRequest request, @RequestParam("userName") String userName, @RequestParam("password") String password) {
         try {
             String sessionHash = PasswordHasher.generateStrongPasswordHash(String.valueOf(UUID.randomUUID()));
             Account loggedInUser = userMgr.login(userName, password, sessionHash);
             if (loggedInUser != null) {
-                response.addCookie(new Cookie("User", String.valueOf(loggedInUser.getId())));
-                response.addCookie(new Cookie("Session", sessionHash));
+                Cookie userCookie = new Cookie("User", String.valueOf(loggedInUser.getId()));
+                Cookie sessionCookie = new Cookie("Session", sessionHash);
+                userCookie.setPath("/");
+                sessionCookie.setPath("/");
+                response.addCookie(userCookie);
+                response.addCookie(sessionCookie);
+
+                Cookie[] test = request.getCookies();
+                importJson("{\"artist\":\"Jakub Kasper\",\"attack\":4,\"cardClass\":\"NEUTRAL\",\"collectible\":true,\"cost\":4,\"dbfId\":2518,\"flavor\":\"The crowd ALWAYS yells lethal.\",\"health\":4,\"id\":\"AT_121\",\"name\":\"Crowd Favorite\",\"playerClass\":\"NEUTRAL\",\"rarity\":\"EPIC\",\"referencedTags\":[\"BATTLECRY\"],\"set\":\"TGT\",\"text\":\"Whenever you play a card with <b>Battlecry</b>, gain +1/+1.\",\"type\":\"MINION\"}");
                 return "User has logged in succesfully";
             }
         } catch (Exception e) {
             Logger.getLogger(AccountController.class.getName()).log(Level.SEVERE, null, e);
         }
         return "Something went wrong!";
+    }
+
+    @RequestMapping("getCollection")
+    public List<Card> getCollection(HttpServletRequest request) throws AccessDeniedException {
+        if( WebUtils.getCookie(request, "Session").getValue() == userMgr.getUserById(Integer.parseInt(WebUtils.getCookie(request, "User").getValue())).getSession()){
+            return userMgr.getUserById(Integer.parseInt(WebUtils.getCookie(request, "User").getValue())).getCollection();
+        }
+        throw new AccessDeniedException("You'ren't logged in!");
+    }
+
+    @RequestMapping("getUser")
+    public @ResponseBody User getaccount(HttpServletRequest request){
+
+        User account = userMgr.getUserById(Integer.parseInt(WebUtils.getCookie(request, "User").getValue()));
+        return account;
+    }
+
+
+    public void importJson(String Json){
+        JSONObject obj = new JSONObject(Json);
+        Card card = new Card(obj.getString("artist"));
+
     }
 
 }
